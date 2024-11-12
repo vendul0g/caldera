010d65353c50e01bd49c067b5c1d7fdd15b6294f
Álvaro Navarro <a.navarromartinez1@um.es>
2024-11-12 11:08:12 +0100

    Modified parser_api and updated docker-compose

--- app/api/v2/handlers/parser_api.py
+++ app/api/v2/handlers/parser_api.py
@@ -1,5 +1,6 @@
 from aiohttp import web
 import os
+import aiohttp_apispec
 
 class ParserApi:
     def __init__(self, services):
@@ -9,9 +10,31 @@ class ParserApi:
         router = app.router
         router.add_get('/parsers', self.get_parsers)
 
+    @aiohttp_apispec.docs(
+        tags=['parsers'],
+        summary='Retrieve Available Parsers',
+        description='Returns a list of all available parser files located in the stockpile plugin parsers directory.',
+        responses={
+            200: {
+                'description': 'List of parser files in JSON format.',
+                'content': {
+                    'application/json': {
+                        'example': {
+                            'parsers': ['example_parser.py', 'another_parser.py']
+                        }
+                    }
+                }
+            }
+        }
+    )
     async def get_parsers(self, request):
+        """
+        Retrieve a list of available parsers.
+
+        This endpoint scans the 'plugins/stockpile/app/parsers' directory
+        and returns all files ending with `.py`.
+        """
         parser_list = []
-        # Example of accessing parser modules from a specific directory
         parser_dir = 'plugins/stockpile/app/parsers'
         for root, dirs, files in os.walk(parser_dir):
             for file in files:
